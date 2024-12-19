ebc06a4ca0602395bac9ed2fb8a072a408be0f92
tempHero <123@dcyy.com>
2024-12-20 04:12:58 +0800

    Fixed bugs that could not be translated

--- YaDeYaDe/evaluate.py
+++ YaDeYaDe/evaluate.py
@@ -6,6 +6,8 @@ from .parser import args
 from torch.autograd import Variable
 from .utils import subsequent_mask
 
+from nltk import word_tokenize
+
 def log(data, timestamp):
     file = open(f'log/log-{timestamp}.txt', 'a')
     file.write(data)
@@ -54,30 +56,31 @@ def evaluate(data, model):
                     break
             print("translation: %s" % " ".join(translation))
             log("translation: " + " ".join(translation) + "\n", timestamp)
-       
+
+
+
 
 def translate_sentence(model, en_sentence, data, args):
-    # 将输入的英文句子转换为索引
-    src = [data['en_word_dict'].get(w, data['en_word_dict']["UNK"]) for w in en_sentence.split()]
-    src = torch.from_numpy(np.array(src)).long().to(args.device)
-    src = src.unsqueeze(0)
+    # 1. 英文句子的分词和ID化
+    en_tokens = ["BOS"] + word_tokenize(en_sentence.lower()) + ["EOS"]
+    en_ids = [data['en_word_dict'].get(w, data['en_word_dict']['UNK']) for w in en_tokens]
     
-    # 创建源句子的 mask
-    src_mask = (src != 0).unsqueeze(-2)
+    # 2. 创建源语言输入张量并构建掩码
+    src = torch.tensor(en_ids).unsqueeze(0).long().to(args.device)
+    src_mask = (src != 0).unsqueeze(-2)  # 生成源语言的掩码，避免填充部分
     
-    # 使用贪婪解码生成中文翻译
-    out = greedy_decode(model, src, src_mask, max_len=args.max_length, start_symbol=data['cn_word_dict']["BOS"])
-    
-    # 将输出的索引转换回中文单词
+    # 3. 使用贪心解码生成翻译
+    with torch.no_grad():
+        out = greedy_decode(model, src, src_mask, args.max_length, start_symbol=data['cn_word_dict']["BOS"])
+
+    # 4. 将生成的目标语言索引转换为中文
     translation = []
-    for j in range(1, out.size(1)):
-        sym = data['cn_index_dict'][out[0, j].item()]
-        if sym != 'EOS':
-            translation.append(sym)
+    for j in range(1, out.size(1)):  # 跳过BOS标记
+        word = data['cn_index_dict'][out[0, j].item()]
+        if word != 'EOS':
+            translation.append(word)
         else:
             break
     
-    # 返回生成的翻译
+    # 5. 返回翻译的中文句子
     return "".join(translation)
-
-
