2ec43948c4cc321620ad3f56ed7923283a9ad7d0
tempHero <123@dcyy.com>
2024-12-20 18:46:00 +0800

    Test Japanese data sets

--- YaDeYaDe/text_tokenize_tool.py
+++ YaDeYaDe/text_tokenize_tool.py
@@ -1,11 +1,30 @@
 from nltk import word_tokenize
 import jieba
 
-def source_text_tokenize(input_text: str) -> list[str]:
+def zh_text_tokenize(input_text: str) -> list[str]:
+    return [word for word in jieba.cut(input_text)]
+
+
+def en_text_tokenize(input_text: str) -> list[str]:
     return word_tokenize(input_text.lower())
 
 
+def simple_text_tokenize(input_text: str) -> list[str]:
+    return word_tokenize( " ".join([w for w in input_text]) )
+
+
+
+"""
+    预处理调用的两个函数
+"""
+def source_text_tokenize(input_text: str) -> list[str]:
+    return zh_text_tokenize(input_text)
+
 
 def result_text_tokenize(input_text: str) -> list[str]:
-    # return word_tokenize( " ".join([w for w in input_text]) )
-    return [word for word in jieba.cut(input_text)]
+    return simple_text_tokenize(input_text)
+
+
+
+
+
--- run.py
+++ run.py
@@ -5,15 +5,15 @@ from YaDeYaDe.run_utils import *
 
 if __name__ == "__main__":
 
-    # Train(
-    #     train_file_path="data/train.txt", 
-    #     dev_file_path="data/dev.txt",
-    #     save_path='output/model.pt',
-    #     epochs=8
-    # ).start()
+    Train(
+        train_file_path="data/ja/train.txt", 
+        dev_file_path="data/ja/dev.txt",
+        save_path='output/zh_ja/model.pt',
+        epochs=5
+    ).start()
 
 
-    translate = Translate('output/model.pt')
-    for text in ["take care.", "hi!", "who are your?"]:
+    translate = Translate('output/zh_ja/model.pt')
+    for text in ["你好。", "我是谁？", "吃饭了吗？"]:
         res = translate.translate_text(text)
         print(res)
